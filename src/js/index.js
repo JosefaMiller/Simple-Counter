d3b5d09f822914518ed0279634a3460a9a9f90f9
Josefa Miller <josefamillerb@gmail.com>
2023-03-09 12:29:19 -0300

    starting

--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,11 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let tiempo = 0;
+setInterval(() =>{
+    console.log(tiempo);
+    tiempo++
+},1000)
+
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
